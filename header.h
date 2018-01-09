d3e6420cc703dc925f7aa61c196a3fdb2e2886dd
Damousse <damien.billot-mornet@sfr.fr>
2018-01-09 18:53:58 +0100

    first commit

---
+++ header.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAILLE_MAX_LIGNE 400
---
+++ main.c
@@ -0,0 +1,37 @@
+#include "header.h"
+
+int main()
+{
+    FILE* fichier = NULL;
+    char chaine[TAILLE_MAX_LIGNE] = "";
+    char* ptr_comma,ptr_colon;
+
+    fichier = fopen("C:\\Users\\94000187\\Desktop\\projet_en_cours\\CEI\\fichier_txt_db\\part_of_mtlinki_Signal_History.txt", "r");
+
+    if (fichier != NULL)
+    {
+        printf("Fichier ouvert !\n");
+        fgets(chaine,TAILLE_MAX_LIGNE,fichier);
+        //printf("%s",chaine);
+
+        ptr_comma = strtok(chaine,",:");
+
+        while(ptr_comma != NULL){
+            printf("%s\n",ptr_comma);
+
+            //TODO
+
+            ptr_comma = strtok(NULL,",:");
+        }
+
+        fclose(fichier); // On ferme le fichier qui a été ouvert
+    }
+
+    else
+    {
+        // On affiche un message d'erreur si on veut
+        printf("Impossible d'ouvrir le fichier : part_of_mtlinki_Signal_History.txt");
+    }
+
+    return 0;
+}
